e5b0a77b4ed5c1f1f2c784f556f8bfcc70c4773d
ednasolans <edna.solans@gmail.com>
2024-12-24 17:11:18 +0100

    PMVehicle implemented

--- src/main/micromobility/PMVehicle.java
+++ src/main/micromobility/PMVehicle.java
@@ -1,15 +1,39 @@
 package micromobility;
 /**
- * Internal classes involved in  the use of the service
+ * Internal classes involved in the use of the service
  */
+
 public class PMVehicle {
-// ??? // The class members
+
     private PMVState state;
-// ??? // The constructor/s
-    // All the getter methods
-    // The setter methods to be used are only the following ones
-//    public void setNotAvailb () { . . . } throws ProceduralException;
-//    public void setUnderWay () { . . . } throws ProceduralException;
-//    public void setAvailb () { . . . } throws ProceduralException;
-//    public void setLocation (GeographicPoint gP) { . . . }
+    private data.GeographicPoint location;
+
+    public PMVehicle() {
+        this.state = PMVState.Available;
+    }
+
+    public PMVState getState() {
+        return state;
+    }
+
+    public data.GeographicPoint getLocation() {
+        return location;
+    }
+
+    public void setNotAvailb() {
+        this.state = PMVState.NotAvailable;
+    }
+
+    public void setUnderWay() {
+        this.state = PMVState.UnderWay;
+    }
+
+    public void setAvailb() {
+        this.state = PMVState.Available;
+    }
+
+    public void setLocation(data.GeographicPoint location) {
+        this.location = location;
+
+    }
 }
