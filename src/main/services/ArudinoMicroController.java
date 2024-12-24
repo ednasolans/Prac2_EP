a571e5780b3814c77876d2ad080a4aa2998c6ea2
ednasolans <edna.solans@gmail.com>
2024-12-24 17:50:34 +0100

    ArudinoMicroController created

--- src/main/services/ArudinoMicroController.java
+++ src/main/services/ArudinoMicroController.java
@@ -1,4 +1,15 @@
 package services;
 
-public interface ArudinoMicroController {
+public interface ArduinoMicroController { // Software for microcontrollers
+
+    public void setBTconnection ()
+            throws ConnectException;
+
+    public void startDriving ()
+            throws PMVPhisicalException, ConnectException, ProceduralException;
+
+    public void stopDriving ()
+            throws PMVPhisicalException, ConnectException, ProceduralException;
+
+    public void undoBTconnection ();
 }
